acfea35bb8ff2002f7c86cfb4e5a83f67244c05f
jaeguk-cho <37435790+cindy-cho@users.noreply.github.com>
2021-05-26 16:22:39 +0900

    Update 1018_체스판다시칠하기.cpp
    너무 어렵게 생각해서 인지 쉽게 못풀었다. 체스판의 크기가 작으니까 완전탐색으로 개수를 구해줘도 괜찮은듯 싶다. 숫자가 커지면 어떻게 해야할지? 는 고민해봐야 할 부분. DP를 써서 구한 크기는 저장하던가 해야하지 않을까 싶긴하다.

--- Baekjoon/SolvedAC_Class2/1018_체스판다시칠하기.cpp
+++ Baekjoon/SolvedAC_Class2/1018_체스판다시칠하기.cpp
@@ -6,21 +6,39 @@ using namespace std;
 int main(void){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int N,M;
-    int min,tmp;
-    string sample1="WBWBWBWB",sample2="BWBWBWBW";
+    int N,M,cmpIdx;
+    int minAnswer=2500,toColor;
+    string cmp[2]={"BWBWBWBW","WBWBWBWB"};
     cin >> N >> M;
-    min = N*M;
     vector <string> chess (N);
     for(int i=0;i<N;i++){
         cin>> chess[i];
     }
-    for(int i=0;i<=N-8;i++){
-        for(int j=0;j<=M-8;j++){
-            for(int k=0;k<8;k++){
-                
+
+    for(int i=0;i<N-7;i++){
+        for(int j=0;j<M-7;j++){
+            toColor = 0;
+            cmpIdx = 0;
+            for(int a=0;a<8;a++){
+                for(int b=0;b<8;b++){
+                    if(cmp[cmpIdx][b]!=chess[i+a][j+b]) toColor++;
+                }
+                cmpIdx = (cmpIdx+1)%2;
             }
+            if(minAnswer > toColor) minAnswer = toColor;
+
+            toColor = 0;
+            cmpIdx = 1;
+            for(int a=0;a<8;a++){
+                for(int b=0;b<8;b++){
+                    if(cmp[cmpIdx][b]!=chess[i+a][j+b]) toColor++;
+                }
+                cmpIdx = (cmpIdx+1)%2;
+            }
+            if(minAnswer > toColor) minAnswer = toColor;
         }
     }
+
+    cout << minAnswer << "\n";
     return 0;
 }
