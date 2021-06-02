a1305d811a00d503d84e0e541f1274943dbec3b4
yoohwa-cho <43ryuflower@gmail.com>
2021-06-02 13:03:33 +0900

    binary search with upper bound
    binary search - upper bound
    정올 1219

--- 정올/Intermediate Coder/분할정복/1219_모자이크.cpp
+++ 정올/Intermediate Coder/분할정복/1219_모자이크.cpp
@@ -1,16 +1,54 @@
 #include<iostream>
 #include<vector>
-#define endl '\n'
+#include<algorithm>
+#define endl "\n"
 typedef long long ll;
 
 using namespace std;
 
+bool cmp(pair<ll,ll> A, pair<ll, ll> B){
+    if(A.second == B.second) return A.first < B.first;
+    return A.second < B.second;
+}
 int main(void){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int row, col, paper, wrong, pR, pC;
-    cin >> row >> col >> paper >> wrong;
-    vector<ll, ll> arr(wrong);
-    for(int i=0;i<wrong;i++) cin >> arr[i].first >> arr[i].second ;
+    int row, col, paper, wrongColor, cnt, stand,idx;
+    long long start=0,end=1000000,mid,ans=0;
+    cin >> row >> col >> paper >> wrongColor;
+    vector<pair<ll, ll>> arr(wrongColor);
+    for(int i=0;i<wrongColor;i++) {
+        cin >> arr[i].first >> arr[i].second ;
+        if(start < arr[i].first) start = arr[i].first;
+    }
+
+    // cout << "Start , End : " << start << "," << end << endl;
+    sort(arr.begin(), arr.end(),cmp);
+    // for(int i=0;i<wrongColor;i++) cout << arr[i].second << "," << arr[i].first << endl;
+    if(wrongColor==0){
+        cout << "0\n";
+        return 0;
+    }
+    while(start<=end){
+        mid = (start+end)/2;
+        cnt = 0;
+        stand = arr[0].second;
+        idx = 0;
+        while(idx < wrongColor){
+            cnt++;
+            while(arr[idx].second<stand+mid){
+                idx++;
+                if(idx == wrongColor) break;
+            }
+            if(idx == wrongColor) break;
+            stand = arr[idx].second;
+        }
+        if(cnt>paper)start = mid+1;
+        else {
+            ans = mid;
+            end = mid-1;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
