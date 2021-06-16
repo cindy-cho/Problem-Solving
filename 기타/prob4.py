bdb498c6dee5d60381eef615d43793979caa1d45
yoohwa-cho <43ryuflower@gmail.com>
2021-06-17 01:37:42 +0900

    Update prob4.py

--- 기타/prob4.py
+++ 기타/prob4.py
@@ -8,30 +8,30 @@ attendanceScore = float(input("출석 점수:"))/15*30
 
 totalScore= midtermScore+finalScore+homeworkScore+quizScore+attendanceScore
 
-if totalScore <= 100 and totalScore > 95 :
+if totalScore <= 100 and totalScore >= 95 :
     print("프통문 최종 점수는 %.2f 이고 예상 학점은 A+ 입니다." %(totalScore))
 
-elif totalScore <= 100 and totalScore > 95 :
+elif totalScore < 95 and totalScore >= 90 :
     print("프통문 최종 점수는 %.2f 이고 예상 학점은 A 입니다." %(totalScore))
 
-elif totalScore <= 100 and totalScore > 95 :
+elif totalScore < 90 and totalScore >= 85 :
     print("프통문 최종 점수는 %.2f 이고 예상 학점은 B+ 입니다." %(totalScore))
 
-elif totalScore <= 100 and totalScore > 95 :
+elif totalScore < 85 and totalScore >= 80 :
     print("프통문 최종 점수는 %.2f 이고 예상 학점은 B 입니다." %(totalScore))
 
-elif totalScore <= 100 and totalScore > 95 :
+elif totalScore < 80 and totalScore >= 75 :
     print("프통문 최종 점수는 %.2f 이고 예상 학점은 C+ 입니다." %(totalScore))
 
-elif totalScore <= 100 and totalScore > 95 :
+elif totalScore < 75 and totalScore >= 70 :
     print("프통문 최종 점수는 %.2f 이고 예상 학점은 C 입니다." %(totalScore))
 
-elif totalScore <= 100 and totalScore > 95 :
+elif totalScore < 70 and totalScore >= 65 :
     print("프통문 최종 점수는 %.2f 이고 예상 학점은 D+ 입니다." %(totalScore))
 
-elif totalScore <= 100 and totalScore > 95 :
+elif totalScore < 65 and totalScore >= 60 :
     print("프통문 최종 점수는 %.2f 이고 예상 학점은 D 입니다." %(totalScore))
 
-elif totalScore <= 100 and totalScore > 95 :
+elif totalScore < 60:
     print("프통문 최종 점수는 %.2f 이고 예상 학점은 F 입니다." %(totalScore))
 
